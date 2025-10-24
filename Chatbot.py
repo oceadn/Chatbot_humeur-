e6e6a0ef13bebbc34be50f3992eaf66e5edc46ed
oceadn <oceadn@gmail.com>
2025-10-24 17:30:48 +0200

    Mise à jour

--- Chatbot.py
+++ Chatbot.py
@@ -1,38 +1,47 @@
 # Exercice : créer un chatbot
 
-import random # toujours en début de fichier !
+import random  # toujours en début de fichier !
 
-print("=== Assitant Moral - Premier programme ===")
+print("=== Assistant Moral - Premier programme ===")
 
 name = input("Quel est ton prénom ? ")
 print("\nEnchanté(e)", name, "!\nJe suis ravie de te connaître.")
 
-mood = input("\nComment te sens-tu aujourd'hui? (bien / moyen / mal) ")
+mood = input("\nComment te sens-tu aujourd'hui ? (bien / moyen / mal / fatigué(e)) ")
 
 def repondre(mood, name):
- if mood == "bien":
-      print(random.choice([
+    if mood == "bien":
+        print(random.choice([
             "Super ! Je suis contente pour toi.",
             "Génial ! Profite de cette bonne énergie.",
             "Top ! Continue sur cette lancée."
-            ]))
-      
- elif mood == "moyen":
-      print(random.choice([
+        ]))
+
+    elif mood == "moyen":
+        print(random.choice([
             f"Courage, {name}. Un pas à la fois.",
             "De belles choses t'attendent.",
-            "Prend un morceau de chocolat. (^_-)"
-            ]))
+            "Prends un morceau de chocolat. (^_-)"
+        ]))
 
- elif mood == "mal":
-      print(random.choice([
+    elif mood == "mal":
+        print(random.choice([
             f"Je suis là, {name}. Tu n'es pas seul(e).",
             "Une chose à la fois. Tu vas t'en sortir.",
-            "As-tu quelque chose pour te réconforter?"
-            ]))
- else:
-      print("Je n'ai pas bien compris ton humeur, peux-tu recommencer?")
+            "As-tu quelque chose pour te réconforter ?"
+        ]))
+
+    elif mood == "fatigué" or mood == "fatiguée":
+        print(random.choice([
+            "Tu as bien travaillé pour aujourd'hui.",
+            "Tu mérites une bonne nuit de sommeil.",
+            "Les personnes intelligentes sont toujours fatiguées."
+        ]))
+
+    else:
+        print("Je n'ai pas bien compris ton humeur, peux-tu recommencer ?")
 
 repondre(mood, name)
-      
-print("\nPasse une belle journée, ", name, "!")
+
+print("\nPasse une belle journée,", name, "!")
+
